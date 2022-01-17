02b190efc22b87d043f6d329256510ab6fbdcd2b
filipanatalio <87820213+filipanatalio@users.noreply.github.com>
2022-01-17 18:48:21 +0000

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -18,3 +18,20 @@ if (hacker1.length > hacker2.length) {
   );
 }
 // Iteration 3: Loops
+let driverNameInUpperCase = "";
+for (letter of hacker1) {
+  driverNameInUpperCase += letter.toUpperCase() + " ";
+}
+console.log(driverNameInUpperCase);
+let navigatorReverseName = "";
+for (i = hacker2.length - 1; i >= 0; i--) {
+  navigatorReverseName += hacker2[i];
+}
+console.log(navigatorReverseName);
+if (hacker1.localeCompare(hacker2) === -1) {
+  console.log(`The driver's name goes first.`);
+} else if (hacker1.localeCompare(hacker2) === 1) {
+  console.log("The navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
